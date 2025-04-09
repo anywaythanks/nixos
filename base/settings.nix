c83023c722b87c16be637549c3138056c4a03b8a
kabeshev-kd-241113 <anywaythanks@mail.ru>
2025-04-09 11:57:44 +0300

    +x

--- base/settings.nix
+++ base/settings.nix
@@ -76,6 +76,18 @@
     };
   };
 
+  #Музика
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  hardware.bluetooth.enable = true;
   #драйвера
   hardware = {
     opengl = {
@@ -89,7 +101,6 @@
       ];
       # driSupport = true;
     };
-    pulseaudio.enable = true;
   };
 
   #Тырнет
