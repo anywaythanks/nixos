e6c65d435c07786d6162ed0487e992263cd9eeff
kabeshev-kd-241113 <anywaythanks@mail.ru>
2025-09-02 01:12:34 +0300

    upd

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
         #nixpkgs.overlays = [ taffybar.overlays.default ];
         # Let Home Manager install and manage itself.
         programs.home-manager.enable = true;
-        home.stateVersion = "24.05";
+        home.stateVersion = "25.05";
         imports = [
           ./modules/bat
           ./modules/cli.nix
